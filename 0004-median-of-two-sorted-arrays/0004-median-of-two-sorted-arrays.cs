3b21e597dd0b5b3ab2f8686f558edf1b974b16e3
Tukello Cristian <105870343+TukelloMathole@users.noreply.github.com>
2024-08-01 17:38:59 +0200

    Time: 90 ms (75.82%), Space: 54.7 MB (66.82%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cs
@@ -0,0 +1,43 @@
+public class Solution {
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
+       // Ensure nums1 is the smaller array
+        if (nums1.Length > nums2.Length) {
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+        }
+
+        int m = nums1.Length;
+        int n = nums2.Length;
+        int imin = 0, imax = m;
+        int halfLen = (m + n + 1) / 2;
+
+        while (imin <= imax) {
+            int i = (imin + imax) / 2;
+            int j = halfLen - i;
+
+            // Check partition values
+            int maxLeft1 = (i == 0) ? int.MinValue : nums1[i - 1];
+            int minRight1 = (i == m) ? int.MaxValue : nums1[i];
+            int maxLeft2 = (j == 0) ? int.MinValue : nums2[j - 1];
+            int minRight2 = (j == n) ? int.MaxValue : nums2[j];
+
+            if (maxLeft1 <= minRight2 && maxLeft2 <= minRight1) {
+                // Found the correct partitions
+                if ((m + n) % 2 == 0) {
+                    return (Math.Max(maxLeft1, maxLeft2) + Math.Min(minRight1, minRight2)) / 2.0;
+                } else {
+                    return Math.Max(maxLeft1, maxLeft2);
+                }
+            } else if (maxLeft1 > minRight2) {
+                // Move left in nums1
+                imax = i - 1;
+            } else {
+                // Move right in nums1
+                imin = i + 1;
+            }
+        }
+
+        throw new InvalidOperationException("Input arrays are not sorted correctly."); 
+    }
+}
